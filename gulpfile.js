b62b1fde593918f766278a1a7c4fb5b360923543
Guylian Cox <gc@madkings.com>
2019-06-14 16:12:42 +0200

    build: update build process to node 10

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,74 @@
 /* eslint-disable import/no-commonjs */
 
-const fs = require('fs');
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const plumber = require('gulp-plumber');
 const babel = require('gulp-babel');
 const del = require('del');
 
+const FRAMEWORK_BABEL_PRESET = `@reworkjs/reworkjs/lib/internals/babel/internal-babel-preset`;
+
 // ====== Lib build ======
 // Transpile to ES5
 
-const libBabelRc = JSON.parse(fs.readFileSync('./.lib.babelrc'));
-
-gulp.task('clean-lib', () => {
+function cleanLib() {
   return del(['./lib']);
-});
+}
 
-gulp.task('copy-lib', ['clean-lib'], () => {
+// ['clean-lib']
+function copyLib() {
   return gulp
     .src('./src/**/*')
     .pipe(gulp.dest('./lib'));
-});
+}
 
-gulp.task('build-lib', ['copy-lib'], () => {
+function compileLib() {
   return gulp.src('./src/**/*.js')
     .pipe(plumber())
-    .pipe(babel(libBabelRc))
+    .pipe(babel({
+      presets: [
+        [FRAMEWORK_BABEL_PRESET, { buildEsModules: true }],
+      ],
+    }))
     .pipe(plumber.stop())
     .pipe(gulp.dest('./lib'));
-});
+}
+
+const buildLib = gulp.series(cleanLib, copyLib, compileLib);
 
 // ====== ES  build ======
 // Like Lib but doesn't transform import statements.
 
-const esBabelRc = JSON.parse(fs.readFileSync('./.es.babelrc'));
-
-gulp.task('clean-es', () => {
+function cleanEs() {
   return del(['./es']);
-});
+}
 
-gulp.task('copy-es', ['clean-es'], () => {
+function copyEs() {
   return gulp
     .src('./src/**/*')
     .pipe(gulp.dest('./es'));
-});
+}
 
-gulp.task('build-es', ['copy-es'], () => {
+function compileEs() {
   return gulp.src('./src/**/*.js')
     .pipe(plumber())
-    .pipe(babel(esBabelRc))
+    .pipe(babel({
+      presets: [
+        [FRAMEWORK_BABEL_PRESET, { buildEsModules: false }],
+      ],
+    }))
     .pipe(plumber.stop())
     .pipe(gulp.dest('./es'));
-});
+}
+
+const buildEs = gulp.series(cleanEs, copyEs, compileEs);
 
 // ====== Common ======
 
-gulp.task('build', ['build-lib', 'build-es']);
+const buildAll = gulp.parallel([buildEs, buildLib]);
 
-gulp.task('build:watch', ['build'], () => {
-  return watch('./src/**/*', () => {
-    gulp.start('build-es');
-    gulp.start('build-lib');
-  });
-});
+gulp.task('build', buildAll);
+
+gulp.task('build:watch', gulp.series(buildAll, () => {
+  return watch('./src/**/*', buildAll);
+}));
